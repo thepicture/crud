ebec88a89dcc3b1d719ad304167adb8b6cd2cbb6
thepicture <85333076+thepicture@users.noreply.github.com>
2022-10-01 19:24:27 +0500

    feat(animalfacts / index): add delete

--- animalfacts/urls.py
+++ animalfacts/urls.py
@@ -6,4 +6,5 @@ appname = 'animalfacts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:fact_id>/', views.detail, name='detail'),
+    path('delete/<int:fact_id>/', views.delete, name='delete'),
 ]
--- animalfacts/views.py
+++ animalfacts/views.py
@@ -14,5 +14,12 @@ def index(request) -> HttpResponse:
     return HttpResponse(template.render(context, request))
 
 
-def detail(request, fact_id) -> HttpResponse:
+def detail(_, fact_id) -> HttpResponse:
     return HttpResponse('The fact %s' % fact_id)
+
+
+def delete(request, fact_id) -> HttpResponse:
+    AnimalFact.objects.get(pk=fact_id).delete()
+    template = loader.get_template('animalfacts/deleted.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
